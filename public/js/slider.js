9e604fdc4ed845d9215b4697fa208139377c0f94
Unknown <mike.dalmaijer.362@outlook.com>
2017-11-21 09:48:47 +0100

    Update Master Template

---
+++ public/js/slider.js
@@ -0,0 +1,17 @@
+var slideIndex = 0;
+
+carouse1();
+
+
+function carouse1() {
+	var i;
+	var x = document.getElementsByClassName("mySlides");
+
+	for (i = 0; i < x.length; i++) {
+		x[i].style.display = "none";
+	}
+	slideIndex++;
+	if (slideIndex > x.length) {slideIndex = 1}
+	x[slideIndex-1].style.display = "block";
+	setTimeout(carouse1, 8000);//5 seconds
+}
